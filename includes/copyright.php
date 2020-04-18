badc73aa32c85acbb4246cf0bf4a48ad343f40a2
Sam <super8989@gmail.com>
2020-04-18 12:34:51 -0400

    basic webpage layout

---
+++ includes/copyright.php
@@ -0,0 +1,5 @@
+<h4>Code Dynamic Websites with PHP</h4>
+<h5>Copyright &copy;
+  <?php echo date('Y'); ?>
+  <a href="#">Sam</a> - <a href="https://github.com/super8989" target="_blank">Github</a>
+</h5>
