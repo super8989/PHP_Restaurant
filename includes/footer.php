c156a18d41deb70055edb27d239772db5a033078
Sam <super8989@gmail.com>
2020-04-18 20:23:54 -0400

    Templating a global header and footer

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+<div id="footer" class="cf">
+  <div class="column three">
+    <strong>Phone</strong>
+    888.529.3819
+  </div>
+
+  <div class="column three">
+    <strong>Location</strong>
+    123 Kaio Blvd<br>
+    Toronto, ON
+  </div>
+
+  <div class="column three last">
+    <strong>Hours</strong>
+    <em>Monday - Thursday</em><br>
+    1 PM - 9 PM<br><br>
+
+    <em>Friday - Saturday</em><br>
+    4 PM - 11 PM<br><br>
+
+    <em>Sunday</em><br>
+    Closed<br><br>
+
+  </div>
+
+</div>
+<!--footer-->
+<small>&copy; 2020 Sam</small>
+
+</div>
+<!--content-->
+
+</div>
+<!--banner-->
+
+</div><!-- wrapper-->
+
+<div class="copyright-info">
+  <?php include 'includes/copyright.php' ?>
+</div>
+
+
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php
+include 'includes/arrays.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Franklin's Restaurant</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body id='final-example'>
+  <div class="wrapper">
+    <div id="banner">
+      <a href="/" title="Return to Home">
+        <img src="img/banner.png" alt="Franklin's Fine Dining">
+      </a>
+      <div id="nav">
+        <?php include 'includes/nav.php' ?>
+      </div>
+      <!--nav-->
+
+      <div class="content">
