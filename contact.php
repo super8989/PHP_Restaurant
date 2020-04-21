7afe37a76ec0d2303f966a7c62495d1215ff7bde
Sam <super8989@gmail.com>
2020-04-21 13:21:55 -0400

    simple form validation and submission part 2

--- contact.php
+++ contact.php
@@ -70,13 +70,52 @@ include('includes/header.php');
       die(); // if true, kill the script
     }
 
-    if (!$name || !$email || $msg) {
+    if (!$name || !$email || !$msg) {
       echo '<h4 class="error">All fields required</h4><a href="contact.php" class="button block">Go back and try again</a>';
       exit;
     }
-  }
 
+    // Add the recipient email to a variable
+    $to = "super8989@gmail.com";
+
+    // Create a subject
+    $subject = "$name sent you a message via your contact form";
+
+    // Construct the message
+    $message = "Name: $name\r\n";
+    $message .= "Email: $email\r\n\r\n";
+    $message .= "Message:\r\n$msg";
+
+    // If the subscribe checkbox was checked
+    if (isset($_POST['subscribe']) && $_POST['subscribe'] == 'Subscribe') {
+
+      // Add a new line to the $message
+      $message .= "\r\n\r\nPlease add $email to the mailing list. \r\n";
+    }
+
+    $message = wordwrap($message, 72);
+
+    // Se the mail headers into a variable
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+    $headers .= "From: $name <$email> \r\n";
+    $headers .= "X-Priority: 1\r\n";
+    $headers .= "X-MSMail-Priority: High\r\n\r\n";
+
+    //Send the email!
+    mail($to, $subject, $message, $headers);
   ?>
+
+  <!-- Show success message after email has sent -->
+
+  <h5>Thanks for contacting Franklin's</h5>
+  <p>Please allow 24 hours for a response.</p>
+  <p><a href="/index.php" class='button block'>&laquo; Go to Home Page</a></p>
+
+  <?php
+  } else {
+  ?>
+
 
   <form method="post" action="" id='contact-form'>
     <label for="name">Your name</label>
@@ -94,7 +133,13 @@ include('includes/header.php');
     <input type="submit" class='button next' name='contact_submit' value='Send Message'>
 
   </form>
-</div>
+
+  <?php
+  }
+  ?>
+
+  <hr>
+</div> <!-- contact -->
 
 
 <?php include('includes/footer.php'); ?>
